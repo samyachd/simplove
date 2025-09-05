894cc86b2ce65b3745b9d9a779d3eb09917941c6
[loicgoi] <g.loic.cours@gmail.com>
2025-09-05 14:30:01 +0200

    Ajout navbar base hatml et extension aux index html de chaque app

--- core/views.py
+++ core/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def base(request):
-    return render(request, "base.html")
+def home(request):
+    return render(request, "core/index.html")
