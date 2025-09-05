79ae5413026fd3ba9b5bb266a8d158ec390d60f3
[loicgoi] <g.loic.cours@gmail.com>
2025-09-05 11:35:41 +0200

    mise à jour environnement + .env

--- config/urls.py
+++ config/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('users.urls')),
-    path('', include('matches.urls')),
-    path('', include('profiles.urls')),
-    path('', include('messages.urls')),
+    path("admin/", admin.site.urls),
+    path("user/", include("users.urls")),
+    path("matches/", include("matches.urls")),
+    path("profiles/", include("profiles.urls")),
+    path("messages/", include("messages.urls")),
 ]
--- messages/apps.py
+++ messages/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class MessagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'messages'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "messages"
+    label = "user_messages"
