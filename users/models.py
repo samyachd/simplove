d50d128210b1c5c855332aed1a4121fe1d58d17d
samyachd <achd.samy@gmail.com>
2025-09-08 16:41:56 +0200

    tests pour models et database

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
 
     LANGUAGE_CHOICES = [("FR", "French"), ("EN", "English"), ("ES", "Spanish"), ("DE", "German")]
 
@@ -15,14 +15,8 @@ class Profile(models.Model):
         help_text="Language", 
         default="FR")
     
-    first_name = models.CharField(blank=False, max_length=50)
-    
-    last_name = models.CharField(blank=False, max_length=50)
-    
     phone_number = models.CharField(blank=True, 
                                     null=True, 
                                     help_text="Numéro de téléphone")
-
-    email = models.EmailField(unique=True)
 
     birth_date = models.DateField()
