98572247ee18595359651c502960e01460305d1d
[loicgoi] <g.loic.cours@gmail.com>
2025-09-09 10:08:35 +0200

    Update manquantes

--- users/views.py
+++ users/views.py
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request):
-    return render(request, "users/index.html")
-=======
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login
 from django.contrib.auth import logout
+
 
 def register_view(request):
     if request.method == "POST":
@@ -27,13 +20,14 @@ def register_view(request):
         form = RegisterForm()
     return render(request, "registration/register.html", {"form": form})
 
+
 def account_view(request):
     if not request.user.is_authenticated:
         return render(request, "registration/login_error.html")
     else:
         return render(request, "registration/account.html", {"User": User.username})
 
+
 def logout_view(request):
     logout(request)
     return redirect("/")
->>>>>>> users
