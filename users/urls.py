43fa014eca80186aa20719667f88cc928d2fdbca
[loicgoi] <g.loic.cours@gmail.com>
2025-09-09 15:01:45 +0200

    page d'accueil avec bouton inscrire et connexion

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
-from .views import account_view, logout_view, register_view
+from .views import account_view, register_view
 
 app_name = "users"
 
 urlpatterns = [
     path("account", account_view, name="users"),
     path("register/", register_view, name="register"),
-    path("logout/", logout_view, name="logout"),
     path("", include("django.contrib.auth.urls")),
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login
-from django.contrib.auth import logout
 
 
 def register_view(request):
@@ -26,8 +25,3 @@ def account_view(request):
         return render(request, "registration/login_error.html")
     else:
         return render(request, "registration/account.html", {"User": User.username})
-
-
-def logout_view(request):
-    logout(request)
-    return redirect("/")
