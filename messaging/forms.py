798699e859677b954b64fd0f9b97df2636885563
samyachd <achd.samy@gmail.com>
2025-09-16 16:50:14 +0200

    modifs css/html pour profil (bio => description) et la boite de message

--- messaging/forms.py
+++ messaging/forms.py
@@ -5,3 +5,15 @@ class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ['content']
+        labels = {"content":""}
+        widgets = {
+            'content': forms.Textarea(
+                attrs={
+                    'class': 'form-control',  # classes CSS (Bootstrap etc.)
+                    'rows': 5,               # hauteur (nombre de lignes)
+                    'cols': 40,              # largeur (nombre de colonnes)
+                    'placeholder': 'Écris ton message…',
+                    'style': 'resize: none;',  # désactiver le redimensionnement
+                }
+            ),
+        }
