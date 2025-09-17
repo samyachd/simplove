343776ed34f0cdb1aac1de320d8cd599f192dc67
samyachd <achd.samy@gmail.com>
2025-09-17 11:00:42 +0200

    is en ==

--- core/views.py
+++ core/views.py
@@ -6,12 +6,12 @@ from users.models import UserAccount
 def core(request):
     if (
         request.user.is_authenticated
-        and UserAccount.objects.get(user=request.user).phone_number is ""
+        and UserAccount.objects.get(user=request.user).phone_number == ""
     ):
         return redirect("users:create_account")
     elif (
         request.user.is_authenticated
-        and MemberProfile.objects.get(user=request.user).looking_for is ""
+        and MemberProfile.objects.get(user=request.user).looking_for == ""
     ):
         return redirect("profiles:create_profile")
     return render(request, "index.html")
